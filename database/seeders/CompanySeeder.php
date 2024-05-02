cf7a39ed5102c0fe6c3fbc50936299f19c0d31b6
Joe Calleya <joe.calleya@havashelia.com>
2024-05-02 15:51:01 +0100

    udemy coder along course

---
+++ database/seeders/CompanySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Facades\DB;
+
+class CompanySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $companies=[];
+        $faker = Faker::create();
+
+        foreach(range(1,10) as $index){
+
+            $company = [
+                'name' =>  $faker->company(),
+                'address' => $faker->address(),
+                'website' => $faker->domainName(),
+                'email' => $faker->email(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+
+        $companies[] = $company;
+        }
+        DB::table('companies')->delete();
+        DB::table('companies')->insert($companies);
+    }
+}
